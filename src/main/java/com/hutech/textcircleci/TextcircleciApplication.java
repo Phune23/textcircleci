3ecfc321a120a8e42f7fe22ca5c77c429ac89eb8
Phune23 <phutranbs23@gmail.com>
2024-06-10 12:33:37 +0700

    add

---
+++ src/main/java/com/hutech/textcircleci/TextcircleciApplication.java
@@ -0,0 +1,13 @@
+package com.hutech.textcircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextcircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextcircleciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hutech/textcircleci/TextcircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.textcircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextcircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
